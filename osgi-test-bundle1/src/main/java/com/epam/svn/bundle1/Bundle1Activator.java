a2a1f6e818089c9520eeecb2b652f8889beee394
Sergii Nesprava <sergii_nesprava@epam.com>
2015-04-22 18:40:12 +0300

    commit with bug

--- osgi-test-bundle1/src/main/java/com/epam/svn/bundle1/Bundle1Activator.java
+++ osgi-test-bundle1/src/main/java/com/epam/svn/bundle1/Bundle1Activator.java
@@ -10,6 +10,8 @@ public class Bundle1Activator implements BundleActivator {
     @Override
     public void start(BundleContext bundleContext) throws Exception {
         System.out.println("Bundle1Activator.start");
+
+        bug
     }
 
     @Override
